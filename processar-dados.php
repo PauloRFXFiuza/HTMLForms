f8047bcd912082ca2b6481f76d68a50c20bca5d5
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-26 10:59:55 -0300

    v2c36

--- processar-dados.php
+++ processar-dados.php
@@ -18,7 +18,11 @@ $banco = 'aula_formulario';
 //Conectar com o banco de dados criado no MySQL
 $conn = new mysqli($server, $usuario, $senha, $banco);
 
+//Verificação se a conexão deu erro
 if ($conn->connect_error) {
     // o sinal de ponto "." é o concatenador do PHP
     die("Falha ao se comunicar com banco de dados: " . $conn->connect_error);
 };
+
+$smtp = $conn->prepare("INSERT INTO mensagens (nome, email, mensagem, data, hora) 
+VALUES (?, ?, ?, ?, ?)");
