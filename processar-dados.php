727cba950c16bbbae98d06f554cb33f09a3996a6
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-26 10:45:43 -0300

    v2c31

--- processar-dados.php
+++ processar-dados.php
@@ -4,3 +4,6 @@
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $mensagem = $_POST['mensagem'];
+
+//Conectar com o banco de dados criado no MySQL
+$conn = new mysqli('localhost', 'root', '', 'aula_formulario');
