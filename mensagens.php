3079da2cbc3fb695dfe48436c780414e28c5c467
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-26 11:59:33 -0300

    v3c12

--- mensagens.php
+++ mensagens.php
@@ -10,20 +10,14 @@ $senhaSecreta = "123";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário de Mensagens</title>
+    <title>Ver Mensagens</title>
     <link rel="stylesheet" href="htmlForms.css">
 </head>
 
 <body>
-    <form action="processar-dados.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" placeholder="Digite seu Nome" required>
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" placeholder="exemplo@provedor.com" required>
-
-        <label for="mensagem">Mensagem:</label>
-        <textarea name="mensagem" rows="7" placeholder="Digite sua Mensagem" required></textarea>
+    <form method="post">
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" placeholder="Digite sua Senha" required>
 
         <button type="submit">Enviar</button>
     </form>
